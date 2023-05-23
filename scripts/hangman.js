82d8ddf24db8125ddff9f5cb23553e9eede68e5a
Mihaela Hristova <106870022+Mihaela0278@users.noreply.github.com>
2023-05-23 10:36:45 +0300

    Display empty spaces for chosen random word and show letters when clicked

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -56,7 +56,7 @@ const displayOptions = () => {
 // Block all the buttons
 const blocker = () => {
     let optionsButtons = document.querySelectorAll(".options");
-    let letterButtons = document.querySelectorAll(".letters")
+    let letterButtons = document.querySelectorAll(".letters");
 
     // Disable all options
     optionsButtons.forEach((button) => {
@@ -94,12 +94,25 @@ const generateWord = (optionValue) => {
     chosenWord = optionArray[Math.floor(Math.random() * optionArray.length)];
     chosenWord = chosenWord.toUpperCase();
     console.log(chosenWord);
+
+    // Replace every letter with span containing dash
+    let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
+
+    // Display each element as span
+    userInputSection.innerHTML = displayItem;
 }
 
 // Initial function (called when page loads/user press new game )
 const initializer = () => {
     winCount = 0;
     count = 0;
+
+    // Initially erase all content and hide letters and new game button
+    userInputSection.innerHTML = "";
+    optionsContainer.innerHTML = "";
+    letterContainer.classList.add("hide");
+    newGameContainer.classList.add("hide");
+    letterContainer.innerHTML = "";
 
     // For creating letter buttons
     for (let i = 65; i < 91; i++) {
@@ -108,6 +121,46 @@ const initializer = () => {
 
         // Number to ASCII[A-Z]
         button.innerText = String.fromCharCode(i);
+
+        // Character button click
+        button.addEventListener("click", () => {
+            let charArray = chosenWord.split("");
+            let dashes = document.getElementsByClassName("dashes");
+
+            // If array contains clicked value 
+            // replace the matched dash with letter 
+            // else draw on canvas
+            if (charArray.includes(button.innerText)) {
+                charArray.forEach((char, index) => {
+
+                    // If character in array is same as clicked button
+                    if (char === button.innerText) {
+                        // Replace dash with letter
+                        dashes[index].innerText = char;
+                        // Increment counter
+                        winCount += 1;
+                        // If winCount equals word length
+                        if (winCount == charArray.length) {
+                            resultText.innerHTML = `<h2 class='win-msg'>You Win!!!</h2><p>The word was <span>${chosenWord}</span></p>`;
+                            // Block all buttons
+                            blocker();
+                        }
+                    }
+                });
+            }
+            else {
+                // Lose count 
+                count += 1;
+               
+                // Count === 6 (because head,body,left arm, right arm,left leg,right leg)
+                if (count === 6) {
+                    resultText.innerHTML = `<h2 class='lose-msg'>You Lose!!!</h2><p>The word was <span>${chosenWord}</span></p>`;
+                    blocker();
+                }
+            }
+            // Disable clicked button
+            button.disabled = true;
+        });
         letterContainer.append(button);
     }
 
